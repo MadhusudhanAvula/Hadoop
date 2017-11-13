19d801471eda867f4fb62f7ace38f128c984a2bb
MadhusudhanAvula <avula171@gmail.com>
2017-11-12 23:25:01 -0500

    Update Hbase.sh

--- Hbase/Hbase.sh
+++ Hbase/Hbase.sh
@@ -9,3 +9,25 @@ for all memstores of all regions of all column families of all tables. If this l
 Yes GC does impact on memstore and you can actually modify this behavior by using Memstore-local allocation buffer. 
 I would suggest you to read the 3 part article on "Avoiding Full GCs in HBase with MemStore-Local Allocation Buffers" as 
 below : http://www.cloudera.com/blog/2011/02/avoiding-full-gcs-in-hbase-with-memstore-local-allocation-buffers-part-1/
+
+
+scan 'test', {COLUMNS => ['F'],FILTER => \ 
+"(SingleColumnValueFilter('F','u',=,'regexstring:http:.*pdf',true,true)) AND \
+(SingleColumnValueFilter('F','s',=,'binary:2',true,true))"}
+
+echo "scan 'tableName', { COLUMNS => 'cf:cq2', FILTER => \"ValueFilter( =, 'binaryprefix:value2' )\"} " | hbase shell |awk '{print $1 " | | " $4}'
+
+echo "scan 'tableName', { COLUMNS => 'cf:cq1', FILTER => \"ValueFilter( =, 'binaryprefix:value1' )\"} " | hbase shell |grep value2 |awk '{print $1}'
+
+echo "scan 'tableName',{FILTER => \"MultipleColumnPrefixFilter('cq1','cq2')\"}" | hbase shell
+
+echo "scan 'tableName',{FILTER => \"MultipleColumnPrefixFilter('cq1','cq2')\"}" | hbase shell | grep -e value=Y -e value2
+
+scan 'tableName', {FILTER => "SingleColumnValueFilter('cf','cq1',=,'regexstring:value1') AND SingleColumnValueFilter('cf','cq2',=, 'regexstring:value2' )"}  
+scan 'tableName', {FILTER => "MultipleColumnPrefixFilter('cq1', 'cq2') AND SingleColumnValueFilter('cf','cq1',=,'regexstring:value1') AND SingleColumnValueFilter('cf','cq2',=, 'regexstring:value2' )"}  
+
+echo "scan 'tableName'" | hbase shell 
+cf:cq2
+echo "get 'tableName',{DependentColumnFilter('cf','cq1')}" | hbase shell
+
+echo "scan 'tableName', {FILTER => \"(SingleColumnValueFilter('cf','cq1',=,'regexstring:value1',true,true)) AND (SingleColumnValueFilter('cf','cq2',=,'binary:value2',true,true))\"}" | hbase shell
