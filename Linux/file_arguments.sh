7a73f9712c554fbb5ca09abd112b1000afd47445
MadhusudhanAvula <avula171@gmail.com>
2018-02-21 01:18:57 -0500

    Update file_arguments.sh

--- Linux/file_arguments.sh
+++ Linux/file_arguments.sh
@@ -98,4 +98,18 @@ When both, -a <ARRAY> and a variable name <NAME> is given, then the array is set
 
 Of course it's valid to set individual array elements without using -a:
 
+##Most of the GNU tools that make use of regular expressions (regex)
+ [[:alnum:]]  - [A-Za-z0-9]     Alphanumeric characters
+ [[:alpha:]]  - [A-Za-z]        Alphabetic characters
+ [[:blank:]]  - [ \x09]         Space or tab characters only
+ [[:cntrl:]]  - [\x00-\x19\x7F] Control characters
+ [[:digit:]]  - [0-9]           Numeric characters
+ [[:graph:]]  - [!-~]           Printable and visible characters
+ [[:lower:]]  - [a-z]           Lower-case alphabetic characters
+ [[:print:]]  - [ -~]           Printable (non-Control) characters
+ [[:punct:]]  - [!-/:-@[-`{-~]  Punctuation characters
+ [[:space:]]  - [ \t\v\f]       All whitespace chars
+ [[:upper:]]  - [A-Z]           Upper-case alphabetic characters
+ [[:xdigit:]] - [0-9a-fA-F]     Hexadecimal digit characters
+
 
